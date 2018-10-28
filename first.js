42b930fb425da6e4618dfe5e2237d2677b72cedd
james-built <jgbuilt@gmail.com>
2018-10-28 23:10:50 +1300

    in progress working through tutorial

---
+++ first.js
@@ -0,0 +1,16 @@
+var a = 5;
+var b = 5;
+var c = a + b;
+console.log(c);
+// Data types
+// any type
+// assigning a variable an 'any' type is equivalent to opting out of type checking
+var variable = 'string variable';
+// Built-in types
+// includes: number, string, boolean, void, null & undefined
+var num = 1;
+var name = 'James';
+var truth = true;
+// User-defined types
+// includes: enum, class, interface, array & tuple
+// TypeScript supports all features of object-oriented programming
--- first.ts
+++ first.ts
@@ -3,3 +3,21 @@ let b = 5
 let c = a + b
 
 console.log(c)
+
+// Data types
+
+// any type
+// assigning a variable an 'any' type is equivalent to opting out of type checking
+let variable: any = 'string variable'
+
+// Built-in types
+// includes: number, string, boolean, void, null & undefined
+
+let num: number = 1
+let name: string = 'James'
+let truth: boolean = true
+
+// User-defined types
+// includes: enum, class, interface, array & tuple
+
+// TypeScript supports all features of object-oriented programming
